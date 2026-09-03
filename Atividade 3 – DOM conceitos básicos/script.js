3846088e6e2b68cfac4d0be5a80466e6827bd30b
Jrds-121 <jrds.1218@gmail.com>
2026-09-03 17:57:20 -0300

    textos dinâmicos e contador de caracteres

--- Atividade 3 – DOM conceitos básicos/script.js
+++ Atividade 3 – DOM conceitos básicos/script.js
@@ -19,15 +19,46 @@ dec.addEventListener ("click", function(){
     }
 });
 
-// textos dinamicos
+// textos dinamicos / contador de caracteres
 
 let entrada = document.getElementById("entrada");
 let resCarac = document.getElementById("resCarac");
 let textos = document.getElementById("textos");
+let contador2;
+let novoParagrafo;
 
-entrada.onkeydown = function(event){
+entrada.addEventListener ("input", function(){
+    contador2 = entrada.value.replace(/\s/g, "").length;
+    resCarac.textContent = contador2;
+});
+
+entrada.addEventListener ("keydown", function(event){
     if(event.key == "Enter"){
-        textos.innerHTML = entrada.value;
-        entrada.value = "";
+        novoParagrafo = document.createElement("p")
+        novoParagrafo.textContent = entrada.value
+        textos.appendChild(novoParagrafo);
     };
-};
+});
+
+// adicionar novo item
+let addLista = document.getElementById("addLista");
+let seleção = document.getElementById("seleção");
+let lista = document.getElementById("lista");
+let listaOrdenada;
+let listaNOrdenada
+let novaLista;
+
+addLista.addEventListener ("click", function(){
+    if (seleção.value === "ol"){
+        listaOrdenada = document.createElement("ol")
+        listaOrdenada.textContent = seleção.value
+        lista.appendChild(listaOrdenada)
+
+        for (let i=1; i<=3; i++){
+
+        }
+        
+    } else {
+
+    }
+});
